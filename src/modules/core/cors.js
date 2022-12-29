c9d827181ac62907ce6ccefc7abbd9bd3010823e
Mikhail <mike10581@gmail.com>
2022-12-29 08:57:27 -0500

    fix cors

--- src/modules/core/cors.js
+++ src/modules/core/cors.js
@@ -3,7 +3,8 @@
 function cors(app) {
   app.use((req, res, next) => {
     // eslint-disable-line consistent-return
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL);
+    res.header('Access-Control-Allow-Credentials', true);
     res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept, Authorization',
